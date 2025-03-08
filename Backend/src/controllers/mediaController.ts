e9c5409ef9de091fe0e230826763a07cb04b888f
unknown <dabo-st@hotmail.com>
2025-03-07 23:49:53 -0300

    mediaController.ts updated

--- Backend/src/controllers/mediaController.ts
+++ Backend/src/controllers/mediaController.ts
@@ -3,14 +3,16 @@ import { fetchMediaFromExternalAPI } from '../services/apiService';
 
 export const getMedia = async (req: Request, res: Response) => {
   try {
-    const { type, category, year } = req.query;
-
-    // Llamar al servicio que consume una API externa
-    const media = await fetchMediaFromExternalAPI(type as string, category as string, year as string);
+    const { type = 'movie', category = 'oscar', year } = req.query;
+    
+    const media = await fetchMediaFromExternalAPI(
+      type as string,
+      category as string,
+      year as string
+    );
 
     res.status(200).json({ results: media });
   } catch (error) {
-    console.error('Error fetching media:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Error fetching media' });
   }
 };
