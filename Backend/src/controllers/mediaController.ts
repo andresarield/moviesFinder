fe82fdac1b8622645533df67090a592b4b0d09cb
unknown <dabo-st@hotmail.com>
2025-03-11 23:33:49 -0300

    mediaController updated to manage pages

--- Backend/src/controllers/mediaController.ts
+++ Backend/src/controllers/mediaController.ts
@@ -3,15 +3,16 @@ import { fetchMediaFromExternalAPI } from '../services/apiService';
 
 export const getMedia = async (req: Request, res: Response) => {
   try {
-    const { type = 'movie', category = 'oscar', year } = req.query;
-    
+    const { type = 'movie', category = 'oscar', year, page = 1 } = req.query;
+
     const media = await fetchMediaFromExternalAPI(
-      type as string,
+      type as 'movie' | 'tv',
       category as string,
-      year as string
+      year as string,
+      parseInt(page as string, 10) // Convertir a número
     );
 
-    res.status(200).json({ results: media });
+    res.status(200).json(media); // Devuelve toda la respuesta
   } catch (error) {
     res.status(500).json({ error: 'Error fetching media' });
   }
