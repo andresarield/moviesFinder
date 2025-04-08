3ae317d4a155ad2d2a84680a41291f2c81e56e3e
andresarield <dabo-st@hotmail.com>
2025-04-07 23:47:31 -0300

    mediaRoutes updated

--- Backend/src/routes/mediaRoutes.ts
+++ Backend/src/routes/mediaRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getMedia, getMediaDetails, getGenres } from '../controllers/mediaController';
+import { searchMedia } from '../controllers/mediaController';
 
 export const mediaRouter = Router();
 
 mediaRouter.get('/', getMedia); // Lista de medios
 mediaRouter.get('/details/:id', getMediaDetails); // Detalles de un medio
 mediaRouter.get('/genres', getGenres); // Lista de géneros
+mediaRouter.get('/search', searchMedia); // Nueva ruta para búsquedas
