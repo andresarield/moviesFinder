ad7e5d57e1582709cbe8d2f339298db1ac556aa3
andresarield <dabo-st@hotmail.com>
2025-04-10 23:42:52 -0300

    mediaController updated

--- Backend/src/controllers/mediaController.ts
+++ Backend/src/controllers/mediaController.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from 'express';
-import { searchMediaFromTMDB } from '../services/apiService';
+import { fetchTrendingMediaFromTMDB } from '../services/apiService';
 
-// Controlador para búsquedas
-export const searchMedia = async (req: Request, res: Response) => {
+// Controlador para obtener contenido popular
+export const getTrendingMedia = async (req: Request, res: Response) => {
   try {
-    const { query, type } = req.query;
+    const type = req.query.type as 'movie' | 'tv'; // Tipo: movie o tv
+    const timeWindow = req.query.timeWindow as 'day' | 'week'; // Ventana: día o semana
 
-    if (!query || !type || !['movie', 'tv'].includes(type as string)) {
-      return res.status(400).json({ error: 'Consulta inválida' });
+    if (!type || !['movie', 'tv'].includes(type)) {
+      return res.status(400).json({ error: 'Tipo inválido' });
     }
 
-    const results = await searchMediaFromTMDB(query as string, type as 'movie' | 'tv');
+    const results = await fetchTrendingMediaFromTMDB(type, timeWindow || 'day');
     res.status(200).json(results);
   } catch (error) {
-    console.error('Error fetching search results:', error);
+    console.error('Error fetching trending media:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
