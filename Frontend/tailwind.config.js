e1bd756cc8ebb7dfd787f1ca7c4bac62024da03d
andresarield <dabo-st@hotmail.com>
2025-04-17 00:02:42 -0300

    Tailwind for darkmode

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,7 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#007bff',
+                secondary: '#6c757d',
+            },
+        },
     },
     plugins: [],
 };
